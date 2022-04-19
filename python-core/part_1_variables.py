c9ebd22ae6d63e1788045470a465148ddbb416a4
Freedmore Sidume <freedmores@gmail.com>
2022-04-19 11:35:42 +0200

    Updated part1, changed file type from notebook to a simple python file

---
+++ python-core/part_1_variables.py
@@ -0,0 +1,9 @@
+# variables are containers to store data in a program
+rent = 1220
+gas =202.5
+groceries = 305.6
+
+total = rent+gas+groceries
+
+print('Rent:', rent, ' Gas:',gas, ' Groceries:',groceries)
+print('Total:',total)
